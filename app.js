dbcb433f71d81417bc9f20af1eaf3bee999fbc82
Shen Nguyen <shennguyen.redshinobi96@hmail.com>
2021-06-11 13:11:40 +0300

    upload new code

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (res, req) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
+
 module.exports = app;
