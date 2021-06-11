21d7b2b6b67dc0dd63fa47eeccb06fd5804a499c
Shen Nguyen <shennguyen.redshinobi96@hmail.com>
2021-06-11 12:59:40 +0300

    upload new code

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const path = require("path");
-const cors = require("cors");
 const app = express();
 
-app.use(cors);
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 module.exports = app;
